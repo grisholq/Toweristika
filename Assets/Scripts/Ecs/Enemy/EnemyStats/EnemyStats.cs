85d6579ce2f4c225e59bceebb263b1e6b5897bb2
grisholq <drowen2014@mail.ru>
2020-12-14 13:18:25 +0300

    Commit 14.12

---
+++ Assets/Scripts/Ecs/Enemy/EnemyStats/EnemyStats.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Toweristika.Ecs
+{
+    public class EnemyStats
+    {
+
+    }
+}
---
+++ Assets/Scripts/Other/Classes/Percent.cs
@@ -0,0 +1,25 @@
+﻿namespace Toweristika.Other
+{
+    public class Percent
+    {
+        private float percent;
+        const float max = 100;
+        const float min = 0;
+
+        public Percent(float percent)
+        {
+            this.percent = percent;
+        }
+
+        public void SetPercent(float val, float minVal, float maxVal)
+        {
+            percent = val * (minVal + maxVal) / max;
+        }
+
+        public float GetPercentValue(float minVal, float maxVal)
+        {
+            return percent * (minVal + maxVal) / max;
+        }
+    }
+
+}
